e6c616fd4aa83627fd6cf44d4b8eed09f473ee7d
IConanEdogawa <160710144+IConanEdogawa@users.noreply.github.com>
2024-05-13 04:19:25 +0500

    added folders and components

---
+++ MassaUz/src/app/components/subcribe/subcribe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subcribe',
+  standalone: true,
+  imports: [],
+  templateUrl: './subcribe.component.html',
+  styleUrl: './subcribe.component.scss'
+})
+export class SubcribeComponent {
+
+}
